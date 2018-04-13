c1744711179412687284617e9b19be78e01116f2
kumarChaitanya97 <16103244chaitanya@gmail.com>
2018-04-14 01:11:07 +0530

    Local storage feature added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,12 @@
 init();
+document.addEventListener('DOMContentLoaded', getTask);
 
 function addTodo(){
 	$("#add-todo").keypress(function(e){
 		if(e.which === 13){
-			var text = "<li class='todos'><span><i class='fa fa-trash'></i></span>" + $(this).val();
+			var text = "<li class='todos'><span><i class='fa fa-trash'></i></span>" + $(this).val() + "</li>";
 			$("#main-list").append(text);
+			addToLocalStorage($(this).val());
 			$(this).val("");
 		}
 	});
@@ -12,6 +14,7 @@ function addTodo(){
 
 function deleteTodo(){
 	$("#main-list").on("click",".todos span",function(e){
+		deleteFromLocalStorage($(this).parent().text());
 		$(this).parent().fadeOut(function(){
 			$(this).remove();
 		});
@@ -29,6 +32,46 @@ function completed(){
 	$("#main-list").on("click",".todos",function(){
 		$(this).toggleClass("task-completed");
 	});
+}
+
+function addToLocalStorage(todo) {
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.push(todo);
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTask() {
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+		for (let i = 0; i < tasks.length; i++) {
+			let element = tasks[i];
+			var text = "<li class='todos'><span><i class='fa fa-trash'></i></span>" + element + "</li>";
+			$("#main-list").append(text);
+		}
+	}
+}
+
+function deleteFromLocalStorage(todo) {
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+		tasks.forEach(function (task, i) {
+			if(todo === task) {
+				tasks.splice(i, 1);
+			}
+		});
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}
 }
 
 function init(){
